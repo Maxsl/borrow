41a47a4cccd43bfd857f54a2dda1ddff6c3e7c28
Yang Zhongchao <yangzhongchao1011@gmail.com>
2016-06-01 11:07:23 +0800

    修复linux环境money_format重复定义的问题
    1.修复linux环境money_format重复定义的问题

--- App/Common/Common/function.php
+++ App/Common/Common/function.php
@@ -7,8 +7,10 @@ function d2int($d){
 function acount($arr){
 	return count($arr);
 }
-function money_format($m){
-	return number_format($m,2);
+if (!function_exists('money_format')){
+    function money_format($m){
+    	return number_format($m,2);
+    }
 }
 function msubstr($str, $start=0, $length, $charset="utf-8", $suffix=true) {
     if(function_exists("mb_substr")){
